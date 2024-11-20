ef6ed4952defc7b12e97a4e9227605bf47e43696
lay virak <lay.virak@103ditech.com>
2024-11-20 22:06:36 +0700

    submit

--- lib/utils/widget/cusotm_alert_dialog.dart
+++ lib/utils/widget/cusotm_alert_dialog.dart
@@ -14,59 +14,55 @@ Future<void> onShowAlertDialog(
   double? height,
 }) async {
   showDialog<String>(
-      context: context,
-      builder: (BuildContext context) => Container(
-            child: Dialog(
-              child: Container(
-                height: height ?? 150,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).cardColor,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 10),
-                    Text(
-                      title.toString(),
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10, bottom: 15),
-                      child: Text(
-                        description.toString(),
-                        style: Theme.of(context).textTheme.displayMedium,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: CustomButton(
-                            onPressed: primaryButton,
-                            title: primaryLabel,
-                            isOutline: true,
-                            textStyle:
-                                Theme.of(context).textTheme.displayMedium!,
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: CustomButton(
-                            onPressed: secondaryButton,
-                            title: secondaryLabel,
-                            textStyle: Theme.of(context)
-                                .textTheme
-                                .displayMedium!
-                                .copyWith(
-                                  color: Colors.white,
-                                ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+    context: context,
+    builder: (BuildContext context) => Dialog(
+      child: Container(
+        height: height ?? 150,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Text(
+              title.toString(),
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 15),
+              child: Text(
+                description.toString(),
+                style: Theme.of(context).textTheme.displayMedium,
               ),
             ),
-          ));
+            Row(
+              children: [
+                Expanded(
+                  child: CustomButton(
+                    onPressed: primaryButton,
+                    title: primaryLabel,
+                    isOutline: true,
+                    textStyle: Theme.of(context).textTheme.displayMedium!,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: CustomButton(
+                    onPressed: secondaryButton,
+                    title: secondaryLabel,
+                    textStyle:
+                        Theme.of(context).textTheme.displayMedium!.copyWith(
+                              color: Colors.white,
+                            ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
 }
